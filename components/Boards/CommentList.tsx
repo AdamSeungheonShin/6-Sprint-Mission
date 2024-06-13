0d972b0ede556a6cfc1465722e994a9f4f8ed974
AdamSeungheonShin <ssh9809@naver.com>
2024-06-13 14:51:59 +0900

    refactor: 댓글 리스트 컴포넌트 조건부 렌더링 조건문 부분 코드 간소화(수정)

--- components/Boards/CommentList.tsx
+++ components/Boards/CommentList.tsx
@@ -8,29 +8,28 @@ import formatTimeAgo from "@/utils/formatTimeAgo";
 export default function CommentList({ comments }: { comments: Comment[] }) {
   return (
     <>
-      {comments &&
-        comments.map((comment) => {
-          return (
-            <div className="flex flex-col gap-6 mb-6" key={comment.id}>
-              <div>{comment.content}</div>
-              <div className="flex items-center gap-2">
-                <Image
-                  src={ProfileDefault}
-                  width={32}
-                  height={32}
-                  alt="댓글프로필이미지"
-                />
-                <div>
-                  <p className="text-gray-600">{comment.writer.nickname}</p>
-                  <p className="text-gray-400">
-                    {formatTimeAgo(comment.updatedAt)}
-                  </p>
-                </div>
+      {comments?.map((comment) => {
+        return (
+          <div className="flex flex-col gap-6 mb-6" key={comment.id}>
+            <div>{comment.content}</div>
+            <div className="flex items-center gap-2">
+              <Image
+                src={ProfileDefault}
+                width={32}
+                height={32}
+                alt="댓글프로필이미지"
+              />
+              <div>
+                <p className="text-gray-600">{comment.writer.nickname}</p>
+                <p className="text-gray-400">
+                  {formatTimeAgo(comment.updatedAt)}
+                </p>
               </div>
-              <hr className="h-[1px] bg-gray-200" />
             </div>
-          );
-        })}
+            <hr className="h-[1px] bg-gray-200" />
+          </div>
+        );
+      })}
     </>
   );
 }
